d1ccd2e92ed05831d0f182be8a732432d49cab45
Orel Sokolov <orelcokolov@gmail.com>
2014-08-03 11:44:26 +0300

    Enable color when executing rake

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,15 @@ end
 
 
 RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect] # Disable warnings
   end
